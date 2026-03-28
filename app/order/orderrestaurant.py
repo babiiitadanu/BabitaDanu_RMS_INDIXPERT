28e7b9ce49ff07ef52ac8af10d5485bc851b10cf
Babita Danu <babiiitadanu@gmail.com>
2026-03-28 13:56:38 +0530

    changes

--- app/order/orderrestaurant.py
+++ app/order/orderrestaurant.py
@@ -1,17 +1,21 @@
 from app.dashboard.restaurant_menu import RestaurantMenu
 from app.domain.read_write_json import json_methods
+
 SKY_BLUE = "\033[38;5;117m"
 RED = "\033[91m"
 RESET = "\033[0m"
 
 
 class FoodOrder:
-    def __init__(self): 
-        self.file = r"app\database\menu.json" 
-        menu = json_methods() 
-        self.menu = menu.read_json(self.file) 
-        self.order = {}
+    def __init__(self):
+        self.file = r"app\database\menu.json"
+        self.order_file = r"app\database\order.json"
 
+        self.read_write = json_methods()
+        self.menu = self.read_write.read_json(self.file)
+        self.orderjson = self.read_write.read_json(self.order_file)
+
+        self.order = {}
 
     def order_food(self):
         category = input("Enter Category (VEG/NON-VEG): ").upper()
@@ -21,16 +25,20 @@ class FoodOrder:
         try:
             price = int(self.menu[category][meal_time][item])
             qty = int(input("Enter Quantity: "))
-
+ 
             if item in self.order:
                 self.order[item]['qty'] += qty
             else:
                 self.order[item] = {'price': price, 'qty': qty}
 
-            print(RED +f"{item} added to order"+ RESET)
+            print(RED + f"{item} added to order" + RESET)
+
+           
+            self.orderjson = [self.order] 
+            self.read_write.write_json(self.order_file, self.orderjson)
 
         except KeyError:
-            print(RED+"Item not found in menu!"+RESET)
+            print(RED + "Item not found in menu!" + RESET)
         except ValueError:
             print("Invalid quantity!")
 
@@ -38,17 +46,21 @@ class FoodOrder:
         item = input("Enter item to cancel: ")
         if item in self.order:
             del self.order[item]
-            print(f"{item} removed")
-        else:
-            print("Item not found!")
+            print(RED + f"{item} removed from order" + RESET)
 
-    
+           
+            self.orderjson = [self.order] 
+            self.read_write.write_json(self.order_file, self.orderjson)
+        else:
+            print("Item not found in order!")
+
     def run(self):
         while True:
-            print(SKY_BLUE+"\n<> FOOD ORDER MENU <>"+RESET)
+            print(SKY_BLUE + "\n<> FOOD ORDER MENU <>" + RESET)
             print("1. Order Food")
             print("2. Cancel Item")
-            print("3. Exit")
+            print("3. View Current Order")
+            print("4. Exit")
 
             choice = input("Enter choice: ")
 
@@ -56,9 +68,15 @@ class FoodOrder:
                 self.order_food()
             elif choice == "2":
                 self.cancel_item()
-            
             elif choice == "3":
+                if self.order:
+                    print("Current Order:")
+                    for item, details in self.order.items():
+                        print(f"{item}: {details['qty']} x {details['price']} = {details['qty']*details['price']}")
+                else:
+                    print("No items in order.")
+            elif choice == "4":
                 print("Exiting Food Order...")
                 break
             else:
-                print(" Invalid choice, try again!")
+                print("Invalid choice, try again!")
